b9f5859f3adc869feae28679ea7ecd9f647cc906
Bo tang <393120777@qq.com>
2020-06-20 17:44:34 +0800

    Add files via upload

---
+++ MyLib/__init__.py
@@ -0,0 +1,24 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2020Tencent. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File Name: __init__.py
+Author: martintang <martintang@tencent.com>
+Create Time: 2020/06/20 17:20:04
+Brief:      
+"""
+
+import sys
+import os
+import logging
+import time
+import json
+import datetime
+
+from add import Add
+
---
+++ MyLib/add.py
@@ -0,0 +1,26 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+########################################################################
+# 
+# Copyright (c) 2020Tencent. All Rights Reserved
+# 
+########################################################################
+ 
+"""
+File Name: add.py
+Author: martintang <martintang@tencent.com>
+Create Time: 2020/06/20 17:20:33
+Brief:      
+"""
+
+import sys
+import os
+import logging
+import time
+import json
+import datetime
+
+class Add(object):
+    def add(self, a, b):
+        return a + b
+
